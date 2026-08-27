0dbc1854a109a46a2920f0e79044d2ae5e2eb8fd
Fan Zhang <mac325@Fans-Mac-mini.local>
2026-08-27 15:52:32 -0400

    Add serveStatic middleware to worker.mjs to serve all static assets from Cloudflare Worker assets

--- worker.mjs
+++ worker.mjs
@@ -1,6 +1,10 @@
 import { Hono } from 'hono';
+import { serveStatic } from 'hono/cloudflare-workers';
 
 const app = new Hono();
+
+// Serve Static Assets (index.html, signer.html, dashboard.html, js/...)
+app.use('/*', serveStatic({ root: './' }));
 
 const GITHUB_CLIENT_ID = 'Ov23liTMskA0wVZzq2ee';
 const GITHUB_CLIENT_SECRET = '03bc8d55baa1b578e1ccc95787494b25e76c997d';
